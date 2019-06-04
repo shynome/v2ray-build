86b7399299141658e9418ea6cbd70433d5c930c9
shynome <shynome@gmail.com>
2019-06-05 01:35:54 +0800

    指定容器名称

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 set -xe
 v2dir=/v2ray-build/src/v2ray.com/core
 
-docker_id=$(docker run -v $(pwd)/v2ray-core:${v2dir} shynome/v2ray-build //release:v2ray_darwin_amd64_package)
-docker cp ${docker_id}:${v2dir}/bazel-bin/release/*.zip release/
+docker run -ti --name v2ray-build -v $(pwd)/v2ray-core:${v2dir} shynome/v2ray-build //release:v2ray_darwin_amd64_package
+docker cp v2ray-build:${v2dir}/bazel-bin/release/*.zip release/
