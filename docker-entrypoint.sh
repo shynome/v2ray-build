7e30016089c323ea42ddbf39418ffa2a95dbbd5e
shynome <shynome@gmail.com>
2019-06-08 15:57:37 +0800

    改进

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 set -xe
 
+V2CODE=${GOPATH}/code
 V2DIR=${GOPATH}/src/v2ray.com/core
 
-if [ ! -d ${V2DIR}/vendor ]; then
+tar -cC ${V2CODE} . --exclude=vendor --exclude=bazel-* | tar -xC ${V2DIR}
+
+if [ ! -d ${V2CODE}/vendor ]; then
   echo "need v2ray.com/core/vendor but not exsit"
   exit 1;
 fi
 
-tar -cC ${V2DIR}/vendor . | tar -xC ${GOPATH}/src 
+tar -cC ${V2CODE}/vendor . | tar -xC ${GOPATH}/src 
 
 $HOME/bin/bazel build --action_env=GOPATH=$GOPATH --action_env=PATH=$PATH $@
 
